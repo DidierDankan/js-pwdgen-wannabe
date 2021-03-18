78f81a309da091116ff262cf3aa878a75fd16d09
DidierDankan <didi_archer@hotmail.com>
2021-03-18 19:24:35 +0000

    change layout

--- js/MyScript.js
+++ js/MyScript.js
@@ -1,3 +1,4 @@
+//test JS
 console.log('hello')
 // ask the users name
 // ask user surname
@@ -7,14 +8,19 @@ console.log('hello')
 //user data
 //1 get users name
 var userName = prompt('whats your name?');
+console.log(userName);
+
 //2 get users surname
 var userSurName = prompt('whats your surname?');
+console.log(userSurName);
+
 //3 get user favorite color
 var userFavColor = prompt('whats your favorite color?');
-//verify if works
-console.log(userName);
-console.log(userSurName);
 console.log(userFavColor);
 
 //4 gives user his password
-document.getElementById('mypassword').innerHTML = 'Your Password is: ' + userName + userSurName + userFavColor + 21;
+document.getElementById('mypassword').innerHTML = 'Your password is:'
+document.getElementById('box-password').innerHTML = userName + userSurName + userFavColor + 21;
+
+//password alert
+document.getElementById('attention').innerHTML = 'Alert! This password is not safe!!!'
